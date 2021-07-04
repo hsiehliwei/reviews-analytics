efa5a587cbe06935d121ac7d85f62677dbe7d887
Judo hsieh <judo_hsieh@jet-tech.com.tw>
2021-07-04 11:51:50 +0800

    update filter function

--- real.py
+++ real.py
@@ -6,9 +6,19 @@ with open('reviews.txt', 'r') as f:
         count = count + 1 
         if count % 1000 == 0: # %為求餘數
             print(len(data))
+
 print('檔案已經讀取完畢, 總共有', len(data), '筆資料')
 
 sum_len = 0 #累積每筆留言長度的變數
 for d in data:
     sum_len = sum_len + len(d)
+
 print('每筆留言的平均為', sum_len / len(data))
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度<100')
+print(new[0])
+print(new[1])
