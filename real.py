413266832ebd2960cfe4837de7e3cb34c94183ef
Judo hsieh <judo_hsieh@jet-tech.com.tw>
2021-07-04 11:36:45 +0800

    update 20210704_V2 sum_len

--- real.py
+++ real.py
@@ -6,8 +6,9 @@ with open('reviews.txt', 'r') as f:
         count = count + 1 
         if count % 1000 == 0: # %為求餘數
             print(len(data))
-print(len(data))
-print('=================')
-print(data[0])
-print('=================')
-print(data[1])
+print('檔案已經讀取完畢, 總共有', len(data), '筆資料')
+
+sum_len = 0 #累積每筆留言長度的變數
+for d in data:
+    sum_len = sum_len + len(d)
+print('每筆留言的平均為', sum_len / len(data))
